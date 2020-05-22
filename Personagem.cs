a9ad9e43c86bb935d08daeb574d066398ad8d278
Thiago <thiagonunesal1307@gmail.com>
2020-05-22 16:34:42 -0300

    atividade finalizada

---
+++ Personagem.cs
@@ -0,0 +1,12 @@
+namespace PrimeiroPOO
+{
+    public class Personagem
+    {
+        public string nome;
+        public int idade;
+        public string armadura;
+        public float atacar (float forca, float pot) {
+            return forca * pot;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem riri = new Personagem();
+            riri.nome = "Riri Williams";
+            riri.idade = 18;
+            riri.armadura = "Iron Heart";
+            Console.WriteLine(riri.nome);
+            Console.WriteLine(riri.idade);
+            Console.WriteLine(riri.armadura);
+            Console.WriteLine("Atacou + "+riri.atacar(50f, 20f));
+        }
+    }
+}
